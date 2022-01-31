abeaf1a6c2094762ea7dbe0725ad62e38072de2c
yilmazelif97 <elifyilmaz587@gmail.com>
2022-01-31 12:18:19 +0300

    error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res) => {
   const user = await User.create(req.body);
@@ -13,44 +13,37 @@ exports.createUser = async (req, res) => {
     res.status(400).json({
       status: false,
       message: "User is not successed",
-      error
+      error,
     });
   }
 };
 
-
 exports.loginUser = async (req, res) => {
-   
-    try{
-
-        const{email, password } = req.body;
-        //kullanıcı var mı kontrolü
-        await User.findOne({email},(err,user)=>{
-            //kulllanıcı varsa girilen bilgiler uyuşuyor mu kontrolü
-            if(user){
-                bcrypt.compare(password, user.password,(err, same)=>{
-                    if(same){
-                        //session
-                        req.session.userID = user._id
-                        console.log('login oldu')
-                        console.log(req.session.userID)
-                        
-                        
-                    }
-                   
-                })
-            }
-        })
-
-
-    } catch (error) {
-      res.status(400).json({
-        status: false,
-        message: "User is not successed",
-        
-      });
-      return
-    }
-  };
-
-  
+  try {
+    const { email, password } = req.body;
+    //kullanıcı var mı kontrolü
+    await User.findOne({ email }, (err, user) => {
+      //kulllanıcı varsa girilen bilgiler uyuşuyor mu kontrolü
+      if (user) {
+        bcrypt.compare(password, user.password, (err, same) => {
+          if (same) {
+            //session
+            req.session.userID = user._id;
+            console.log("login oldu");
+            console.log(req.session.userID);
+            return res.send({
+              status:true,
+              message:"giriş oldu"
+            })
+          }
+        });
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: false,
+      message: "User is not successed",
+    });
+    return;
+  }
+};
